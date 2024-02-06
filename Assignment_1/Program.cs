f9add8fa5c3c258cc3673019f125368e136c71d0
Mukul Rai <137355469+rai-mukul@users.noreply.github.com>
2024-02-05 18:07:01 -0600

    Code push

---
+++ Assignment_1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace StarPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pattern A:");
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\nPattern B:\n");
+            for (int i = 10; i >= 1; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\nPattern C:\n");
+            for (int i = 10; i >= 1; i--)
+            {
+                for (int j = 1; j <= 10 - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\nPattern D:\n");
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10 - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
